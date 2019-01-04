317f0568da47c1036d0a9732a175305fbcdb9f79
1586270999 <1586270999@qq.com>
2019-01-04 14:24:27 +0800

    项目初始签入

---
+++ json/tree.js
@@ -0,0 +1,11 @@
+var treeData = [
+    {    "FName": "shandongs",    "FID": "1",    "FParentID": "0"  },
+    {    "FName": "hedong",    "FID": "2",    "FParentID": "0"  },
+    {    "FName": "01",    "FID": "3",    "FParentID": "0"  },
+    {    "FName": "管理员方案1",    "FID": "1000",    "FParentID": "1"  },
+    {    "FName": "管理员方案2",    "FID": "1001",    "FParentID": "1"  },
+    {    "FName": "管理员方案3",    "FID": "1002",    "FParentID": "1"  },
+    {    "FName": "测试方案1",    "FID": "1003",    "FParentID": "2"  },
+    {    "FName": "阿斯达斯",    "FID": "1004",    "FParentID": "3"  },
+    {    "FName": "1",    "FID": "1006",    "FParentID": "2"  }
+];
